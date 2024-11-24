4b0d31bba2d5f0eaa782ce2a4457d26b8afe0f8b
priyampinju <pinjupriyam1826@gmail.com>
2024-11-24 23:10:16 +0530

    tailwind issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
@@ -21,7 +22,32 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-14 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Pinju Priyam
+          </p>
         </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+          />
+        </div>
       </div>
     </nav>
   );
