3fa083ece81c4341ba4492e49062ee207c6a5422
priyampinju <pinjupriyam1826@gmail.com>
2024-11-26 19:09:34 +0530

    canvas position fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,8 +23,23 @@ const Hero = () => {
             interfaces and web applications
           </p>
         </div>
+      </div>
+      <ComputersCanvas />
 
-        <ComputersCanvas />
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="rounded-3xl w-[35px] h-[64px] border-4 border-secondary flex justify-center items-start p-2">
+            <motion.dev
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
       </div>
     </section>
   );
