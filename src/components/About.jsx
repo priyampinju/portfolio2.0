38d8496640a8ceb3d4aefccca0007ed2925ab313
priyampinju <pinjupriyam1826@gmail.com>
2024-11-29 23:44:19 +0530

    about under making

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,8 +16,8 @@ const About = () => {
       <motion.p vaiants={fadeIn("", "", 0.1, 1)} className="">
         I'm Pinu thebrjbdfsdbfs Lorem ipsum dolor amet, adipisicing elit. Eum,
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam
-        molestias amet sunt dignissimos provident, voluptas vel dicta distinctio
-        ducimus similique! autem?
+        molestias amet sunt dignissimos provident, voluptas vel dicta ducimus
+        similique! autem?
       </motion.p>
     </>
   );
