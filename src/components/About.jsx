ba0c9ec9c0da4ba41d6f44b9cebed731c286e992
priyampinju <pinjupriyam1826@gmail.com>
2024-12-02 21:58:00 +0530

    about under making

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
         I'm Pinu thebrjbdfsdbfs Lorem ipsum dolor amet, adipiicing elit. Eum,
         Lorem ipsum, dolor sit amet conseeur adipicg eit. Ull otias amet sunt
         dignissimos provident, voluptas vel dicta ducimus similique! autem?
-        Lorem ipsum dolor sit amet consectetur, adipisicing
+        Lorem ipsum dolor sit amet consectetur
       </motion.p>
     </>
   );
